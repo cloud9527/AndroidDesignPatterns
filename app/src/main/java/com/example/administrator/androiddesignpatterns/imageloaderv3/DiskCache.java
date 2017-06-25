b9d5252fe21dcc7de771e531e4504c26ac58e2c9
cloud9527 <cloud09527@gmail.com>
2017-06-25 17:37:55 +0800

    1.增加SD卡缓存

--- app/src/main/java/com/example/administrator/androiddesignpatterns/imageloaderv3/DiskCache.java
+++ app/src/main/java/com/example/administrator/androiddesignpatterns/imageloaderv3/DiskCache.java
@@ -2,9 +2,8 @@ package com.example.administrator.androiddesignpatterns.imageloaderv3;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Environment;
 
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -20,20 +19,14 @@ public class DiskCache {
     }
 
     public void put(String url, Bitmap bitmap) {
-        File externalStorageDirectory = Environment.getExternalStorageDirectory();
-        File file = new File(externalStorageDirectory, url);
-
-
         FileOutputStream fileOutputStream = null;
         try {
-            file.createNewFile();
-            file.setWritable(true);
-            fileOutputStream = new FileOutputStream(file);
+            fileOutputStream = new FileOutputStream(cacheDir + url);
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
-        } catch (Exception e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         } finally {
-            if (fileOutputStream != null) {
+            if (fileOutputStream!=null){
                 try {
                     fileOutputStream.close();
                 } catch (IOException e) {
@@ -41,18 +34,7 @@ public class DiskCache {
                 }
             }
         }
+
+
     }
-
-    public static void makeRootDirectory(String filePath) {
-        File file = null;
-        try {
-            file = new File(filePath);
-            if (!file.exists()) {
-                file.mkdir();
-            }
-        } catch (Exception e) {
-
-        }
-    }
-
 }
